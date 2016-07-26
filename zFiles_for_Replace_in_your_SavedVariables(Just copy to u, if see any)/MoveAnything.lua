eec60e2da339ee71032e70e4108ccc3554c6277f
Afenar <afenar@gmail.com>
2016-07-26 08:07:03 +0500

    +

---
+++ zFiles_for_Replace_in_your_SavedVariables(Just copy to u, if see any)/MoveAnything.lua
@@ -0,0 +1,463 @@
+
+MADB = {
+	["noMMMW"] = false,
+	["characters"] = {
+	},
+	["alwaysShowNudger"] = false,
+	["frameListRows"] = 18,
+	["profiles"] = {
+		["default"] = {
+			["frames"] = {
+				["VehicleSeatIndicator"] = {
+					["orgPos"] = {
+						"TOPRIGHT", -- [1]
+						"MinimapCluster", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						40, -- [4]
+						15, -- [5]
+					},
+					["name"] = "VehicleSeatIndicator",
+					["pos"] = {
+						"TOPRIGHT", -- [1]
+						"MinimapCluster", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						-699.000366210938, -- [4]
+						171.999877929688, -- [5]
+					},
+				},
+				["CompactRaidFrameManager"] = {
+					["orgPos"] = {
+						"TOPLEFT", -- [1]
+						"UIParent", -- [2]
+						"TOPLEFT", -- [3]
+						-182.000015258789, -- [4]
+						-140.000015258789, -- [5]
+					},
+					["name"] = "CompactRaidFrameManager",
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"UIParent", -- [2]
+						"TOPLEFT", -- [3]
+						-178.929977416992, -- [4]
+						-160.466491699219, -- [5]
+					},
+				},
+				["DraenorZoneAbilityFrame"] = {
+					["orgAlpha"] = 0.949019607843137,
+					["name"] = "DraenorZoneAbilityFrame",
+					["alpha"] = 1,
+					["pos"] = {
+						"BOTTOM", -- [1]
+						"UIParent", -- [2]
+						"BOTTOM", -- [3]
+						-4, -- [4]
+						172.000122070313, -- [5]
+					},
+				},
+				["BNToastFrame"] = {
+					["orgPos"] = {
+						"CENTER", -- [1]
+						"UIParent", -- [2]
+						"CENTER", -- [3]
+						0, -- [4]
+						50, -- [5]
+					},
+					["name"] = "BNToastFrame",
+					["orgScale"] = 1,
+					["scale"] = 1,
+					["orgAlpha"] = 0.949019607843137,
+					["alpha"] = 1,
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"ChatFrame1ButtonFrame", -- [2]
+						"TOPLEFT", -- [3]
+						-420.611250400543, -- [4]
+						827.406280517578, -- [5]
+					},
+				},
+				["QueueStatusMinimapButton"] = {
+					["orgPos"] = {
+						"TOPLEFT", -- [1]
+						"MinimapBackdrop", -- [2]
+						"TOPLEFT", -- [3]
+						22.0000019073486, -- [4]
+						-100, -- [5]
+					},
+					["name"] = "QueueStatusMinimapButton",
+					["scale"] = 1.17700056596236,
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"MinimapBackdrop", -- [2]
+						"TOPLEFT", -- [3]
+						141.019609519087, -- [4]
+						-39.0304506163002, -- [5]
+					},
+					["orgScale"] = 1,
+				},
+				["FocusFrameSpellBar"] = {
+					["orgPos"] = {
+						"TOPLEFT", -- [1]
+						"FocusFrame", -- [2]
+						"BOTTOMLEFT", -- [3]
+						25, -- [4]
+						6.99999952316284, -- [5]
+					},
+					["name"] = "FocusFrameSpellBar",
+					["hidden"] = true,
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						0, -- [4]
+						-4.66666650772095, -- [5]
+					},
+				},
+				["WorldStateCaptureBar1"] = {
+					["name"] = "WorldStateCaptureBar1",
+					["pos"] = {
+						"TOPRIGHT", -- [1]
+						"MinimapCluster", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						-873.576416015625, -- [4]
+						170.6162109375, -- [5]
+					},
+				},
+				["ObjectiveTrackerFrameScaleMover"] = {
+					["orgPos"] = {
+						"TOPRIGHT", -- [1]
+						"MinimapCluster", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						-10, -- [4]
+						0, -- [5]
+					},
+					["name"] = "ObjectiveTrackerFrameScaleMover",
+					["scale"] = 1.02177377395784,
+					["pos"] = {
+						"TOPRIGHT", -- [1]
+						"MinimapCluster", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						-69.8770744403671, -- [4]
+						173.262989942118, -- [5]
+					},
+					["orgScale"] = 1,
+				},
+				["TargetFrameSpellBar"] = {
+					["orgPos"] = {
+						"TOPLEFT", -- [1]
+						"TargetFrame", -- [2]
+						"BOTTOMLEFT", -- [3]
+						25, -- [4]
+						6.99999952316284, -- [5]
+					},
+					["name"] = "TargetFrameSpellBar",
+					["hidden"] = true,
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						-375, -- [4]
+						-491.000030517578, -- [5]
+					},
+				},
+				["MiniMapInstanceDifficulty"] = {
+					["orgPos"] = {
+						"TOPLEFT", -- [1]
+						"MinimapCluster", -- [2]
+						"TOPLEFT", -- [3]
+						22.0000019073486, -- [4]
+						-17, -- [5]
+					},
+					["name"] = "MiniMapInstanceDifficulty",
+					["scale"] = 1,
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"MinimapCluster", -- [2]
+						"TOPLEFT", -- [3]
+						-1214.50512695313, -- [4]
+						-1008.58668518066, -- [5]
+					},
+					["orgScale"] = 1,
+				},
+				["DurabilityFrame"] = {
+					["orgPos"] = {
+						"TOPRIGHT", -- [1]
+						"MinimapCluster", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						-93, -- [4]
+						0, -- [5]
+					},
+					["name"] = "DurabilityFrame",
+					["pos"] = {
+						"TOPRIGHT", -- [1]
+						"MinimapCluster", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						-968.070617675781, -- [4]
+						157.592529296875, -- [5]
+					},
+				},
+				["MultiCastActionBarFrame"] = {
+					["orgPos"] = {
+						"BOTTOMLEFT", -- [1]
+						"MainMenuBar", -- [2]
+						"TOPLEFT", -- [3]
+						30, -- [4]
+						40, -- [5]
+					},
+					["name"] = "MultiCastActionBarFrame",
+					["hidden"] = true,
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						477.999908447266, -- [4]
+						53, -- [5]
+					},
+				},
+				["CompactRaidFrameManagerToggleButton"] = {
+					["orgPos"] = {
+						"RIGHT", -- [1]
+						"CompactRaidFrameManager", -- [2]
+						"RIGHT", -- [3]
+						-9, -- [4]
+						0, -- [5]
+					},
+					["name"] = "CompactRaidFrameManagerToggleButton",
+					["pos"] = {
+						"RIGHT", -- [1]
+						"CompactRaidFrameManager", -- [2]
+						"RIGHT", -- [3]
+						-7.63378810882568, -- [4]
+						-0.993255615234375, -- [5]
+					},
+				},
+				["CastingBarFrame"] = {
+					["orgPos"] = {
+						"BOTTOM", -- [1]
+						"UIParent", -- [2]
+						"BOTTOM", -- [3]
+						0, -- [4]
+						195, -- [5]
+					},
+					["name"] = "CastingBarFrame",
+					["hidden"] = true,
+					["pos"] = {
+						"BOTTOMRIGHT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMRIGHT", -- [3]
+						-862.5, -- [4]
+						152.000015258789, -- [5]
+					},
+				},
+				["WorldStateAlwaysUpFrame"] = {
+					["orgPos"] = {
+						"TOP", -- [1]
+						"UIParent", -- [2]
+						"TOP", -- [3]
+						-5, -- [4]
+						-15, -- [5]
+					},
+					["name"] = "WorldStateAlwaysUpFrame",
+					["pos"] = {
+						"TOP", -- [1]
+						"UIParent", -- [2]
+						"TOP", -- [3]
+						-0.139862060546875, -- [4]
+						-31.7227783203125, -- [5]
+					},
+				},
+				["PetCastingBarFrame"] = {
+					["orgPos"] = {
+						"BOTTOM", -- [1]
+						"UIParent", -- [2]
+						"TOP", -- [3]
+						0, -- [4]
+						-859.999877929688, -- [5]
+					},
+					["name"] = "PetCastingBarFrame",
+					["hidden"] = true,
+					["pos"] = {
+						"BOTTOM", -- [1]
+						"UIParent", -- [2]
+						"TOP", -- [3]
+						0, -- [4]
+						-902.999877929688, -- [5]
+					},
+				},
+				["TimeManagerClockButton"] = {
+					["orgPos"] = {
+						"CENTER", -- [1]
+						"Minimap", -- [2]
+						"CENTER", -- [3]
+						0, -- [4]
+						-75, -- [5]
+					},
+					["name"] = "TimeManagerClockButton",
+					["hidden"] = true,
+					["pos"] = {
+						"BOTTOMLEFT", -- [1]
+						"UIParent", -- [2]
+						"BOTTOMLEFT", -- [3]
+						485.579956054688, -- [4]
+						129.374969482422, -- [5]
+					},
+				},
+				["QueueStatusFrame"] = {
+					["orgPos"] = {
+						"TOPRIGHT", -- [1]
+						"QueueStatusMinimapButton", -- [2]
+						"TOPLEFT", -- [3]
+						0, -- [4]
+						0, -- [5]
+					},
+					["name"] = "QueueStatusFrame",
+					["scale"] = 0.795160282593966,
+					["pos"] = {
+						"TOPRIGHT", -- [1]
+						"QueueStatusMinimapButton", -- [2]
+						"TOPLEFT", -- [3]
+						282.10392547205, -- [4]
+						181.695765742607, -- [5]
+					},
+					["orgScale"] = 1,
+				},
+				["MiniMapMailFrame"] = {
+					["orgPos"] = {
+						"TOPRIGHT", -- [1]
+						"Minimap", -- [2]
+						"TOPRIGHT", -- [3]
+						24, -- [4]
+						-37, -- [5]
+					},
+					["orgAlpha"] = 0.949019607843137,
+					["orgScale"] = 1,
+					["scale"] = 1.1117899984695,
+					["name"] = "MiniMapMailFrame",
+					["alpha"] = 1,
+					["pos"] = {
+						"CENTER", -- [1]
+						"Minimap", -- [2]
+						"CENTER", -- [3]
+						75.7823179522767, -- [4]
+						27.4422741618659, -- [5]
+					},
+				},
+				["SpellActivationOverlayFrame"] = {
+					["orgPos"] = {
+						"CENTER", -- [1]
+						"UIParent", -- [2]
+						"CENTER", -- [3]
+						0, -- [4]
+						0, -- [5]
+					},
+					["name"] = "SpellActivationOverlayFrame",
+					["scale"] = 0.950031337143859,
+					["pos"] = {
+						"CENTER", -- [1]
+						"UIParent", -- [2]
+						"CENTER", -- [3]
+						-4.3087670497677, -- [4]
+						-4.30846626555952, -- [5]
+					},
+					["orgScale"] = 1,
+				},
+				["GarrisonLandingPageMinimapButton"] = {
+					["orgPos"] = {
+						"TOPLEFT", -- [1]
+						"MinimapBackdrop", -- [2]
+						"TOPLEFT", -- [3]
+						32, -- [4]
+						-118.000007629395, -- [5]
+					},
+					["name"] = "GarrisonLandingPageMinimapButton",
+					["scale"] = 0.979313003345388,
+					["pos"] = {
+						"CENTER", -- [1]
+						"Minimap", -- [2]
+						"CENTER", -- [3]
+						82.2660760963117, -- [4]
+						-62.8423036085416, -- [5]
+					},
+					["orgScale"] = 1,
+				},
+				["MirrorTimer1"] = {
+					["orgPos"] = {
+						"TOP", -- [1]
+						"UIParent", -- [2]
+						"TOP", -- [3]
+						0, -- [4]
+						-96, -- [5]
+					},
+					["name"] = "MirrorTimer1",
+					["hidden"] = true,
+					["pos"] = {
+						"TOP", -- [1]
+						"UIParent", -- [2]
+						"TOP", -- [3]
+						-3.05175781250e-005, -- [4]
+						-95.9998779296875, -- [5]
+					},
+				},
+				["GameTimeFrame"] = {
+					["orgPos"] = {
+						"TOPRIGHT", -- [1]
+						"Minimap", -- [2]
+						"TOPRIGHT", -- [3]
+						20, -- [4]
+						-2, -- [5]
+					},
+					["name"] = "GameTimeFrame",
+					["scale"] = 0.899999074935296,
+					["pos"] = {
+						"CENTER", -- [1]
+						"Minimap", -- [2]
+						"CENTER", -- [3]
+						95.5607815985757, -- [4]
+						67.0374746883413, -- [5]
+					},
+					["orgScale"] = 1,
+				},
+				["GuildInstanceDifficulty"] = {
+					["orgPos"] = {
+						"TOPLEFT", -- [1]
+						"MinimapCluster", -- [2]
+						"TOPLEFT", -- [3]
+						22.0000019073486, -- [4]
+						-17, -- [5]
+					},
+					["name"] = "GuildInstanceDifficulty",
+					["scale"] = 0.990837294262251,
+					["pos"] = {
+						"TOPLEFT", -- [1]
+						"MinimapCluster", -- [2]
+						"TOPLEFT", -- [3]
+						-1225.63311136423, -- [4]
+						-1018.42474423763, -- [5]
+					},
+					["orgScale"] = 1,
+				},
+				["PlayerPowerBarAltMover"] = {
+					["orgPos"] = {
+						"BOTTOM", -- [1]
+						"UIParent", -- [2]
+						"BOTTOM", -- [3]
+						0, -- [4]
+						155, -- [5]
+					},
+					["name"] = "PlayerPowerBarAltMover",
+					["pos"] = {
+						"BOTTOM", -- [1]
+						"UIParent", -- [2]
+						"BOTTOM", -- [3]
+						0.000244140625, -- [4]
+						186.000106811523, -- [5]
+					},
+				},
+			},
+		},
+	},
+	["tooltips"] = true,
+	["playSound"] = false,
+	["noBags"] = false,
+	["collapsed"] = true,
+	["closeGUIOnEscape"] = false,
+}
