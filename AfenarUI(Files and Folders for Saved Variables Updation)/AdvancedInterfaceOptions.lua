f2ae5ffc36169cd4e7e86f5a84c06d33af13b5eb
Afenar <afenar@gmail.com>
2016-08-10 01:47:34 +0500

    Added files for AfenarUI Interface and Saved Variables

---
+++ AfenarUI(Files and Folders for Saved Variables Updation)/AdvancedInterfaceOptions.lua
@@ -0,0 +1,2 @@
+
+AdvancedInterfaceOptionsSaved = nil
---
+++ AfenarUI(Files and Folders for Saved Variables Updation)/MikScrollingBattleText.lua
@@ -0,0 +1,627 @@
+
+MSBTProfiles_SavedVars = {
+	["profiles"] = {
+		["Default"] = {
+			["mergeSwingsDisabled"] = true,
+			["hideSkills"] = true,
+			["block"] = {
+				["trailer"] = "<blocked: %a>",
+			},
+			["triggers"] = {
+				["MSBT_TRIGGER_KILL_SHOT"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_DECIMATION"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_ELUSIVE_BREW"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_BERSERK"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_SWORD_AND_BOARD"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_RIME"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_THE_ART_OF_WAR"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_VITAL_MISTS"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_FINGERS_OF_FROST"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_MANA_TEA"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_BLINDSIDE"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_TASTE_FOR_BLOOD"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_LOW_HEALTH"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_LOCK_AND_LOAD"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_TIDAL_WAVES"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_ULTIMATUM"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_NIGHTFALL"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_LAVA_SURGE"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_SUDDEN_DEATH"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_EXECUTE"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_SHOOTING_STARS"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_LOW_PET_HEALTH"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_MOLTEN_CORE"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_BRAIN_FREEZE"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_MISSILE_BARRAGE"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_VICTORY_RUSH"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_SHADOW_INFUSION"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_CLEARCASTING"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_PREDATORS_SWIFTNESS"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_MAELSTROM_WEAPON"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_PVP_TRINKET"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_HAMMER_OF_WRATH"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_REVENGE"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_BLOODSURGE"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_LOW_MANA"] = {
+					["disabled"] = true,
+				},
+				["MSBT_TRIGGER_KILLING_MACHINE"] = {
+					["disabled"] = true,
+				},
+			},
+			["hotThrottleDuration"] = 0,
+			["critOutlineIndex"] = 3,
+			["animationSpeed"] = 30,
+			["dotThrottleDuration"] = 0,
+			["critFontSize"] = 18,
+			["powerThrottleDuration"] = 0,
+			["enableBlizzardDamage"] = true,
+			["glancing"] = {
+				["trailer"] = "<glncd>",
+			},
+			["hideFullOverheals"] = true,
+			["crushing"] = {
+				["trailer"] = "<crshd>",
+			},
+			["enableBlizzardHealing"] = false,
+			["shortenNumbers"] = true,
+			["throttleList"] = {
+				["Власть льда"] = 1,
+				["Похищение жизни"] = 1,
+				["Темное лечение"] = 1,
+				["Прикосновение вампира"] = 1,
+				["Отражающий щит"] = 1,
+				["Объятия вампира"] = 1,
+			},
+			["normalFontSize"] = 16,
+			["stickyCritsDisabled"] = true,
+			["absorb"] = {
+				["trailer"] = "<abs: %a>",
+			},
+			["abbreviateAbilities"] = true,
+			["normalOutlineIndex"] = 2,
+			["hideNames"] = true,
+			["groupNumbers"] = true,
+			["creationVersion"] = "5.7.131",
+			["resist"] = {
+				["trailer"] = "<resisted: %a>",
+			},
+			["critFontName"] = "Arial Narrow",
+			["events"] = {
+				["PET_INCOMING_HOT_CRIT"] = {
+					["message"] = "(%s - %n) * +%a",
+				},
+				["OUTGOING_SPELL_BLOCK"] = {
+					["message"] = "Block! (%s)",
+				},
+				["NOTIFICATION_BUFF_FADE"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_SPELL_PARRY"] = {
+					["message"] = "(%s) Parry!",
+				},
+				["INCOMING_DEFLECT"] = {
+					["message"] = "Reflect!",
+				},
+				["PET_OUTGOING_SPELL_DAMAGE"] = {
+					["message"] = "* %a (%s)",
+				},
+				["OUTGOING_SPELL_MISS"] = {
+					["message"] = "Miss! (%s)",
+					["scrollArea"] = "Custom1",
+				},
+				["PET_OUTGOING_SPELL_DEFLECT"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_SPELL_IMMUNE"] = {
+					["message"] = "Invuln.! (%s)",
+					["fontSize"] = 14,
+					["outlineIndex"] = 2,
+					["scrollArea"] = "Custom2",
+					["fontName"] = "Friz Quadrata TT",
+					["fontAlpha"] = 100,
+				},
+				["PET_OUTGOING_SPELL_DOT_CRIT"] = {
+					["message"] = "* %a (%s)",
+				},
+				["NOTIFICATION_ALT_POWER_GAIN"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_SPELL_ABSORB"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_SPELL_MISS"] = {
+					["message"] = "(%s)Miss!",
+				},
+				["PET_OUTGOING_HOT_CRIT"] = {
+					["message"] = "* +%a (%s - %n)",
+				},
+				["PET_OUTGOING_DAMAGE"] = {
+					["message"] = "*%a",
+				},
+				["NOTIFICATION_CHI_FULL"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_BLOCK"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_HONOR_GAIN"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_HEAL"] = {
+					["message"] = "* +%a (%s - %n)",
+				},
+				["NOTIFICATION_ITEM_BUFF_FADE"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_MISS"] = {
+					["message"] = "* Miss",
+				},
+				["PET_OUTGOING_HEAL_CRIT"] = {
+					["message"] = "* +%a (%s - %n)",
+				},
+				["NOTIFICATION_LOOT"] = {
+					["message"] = "%a %e (%t)",
+					["fontSize"] = 13,
+					["fontName"] = "Friz Quadrata TT",
+				},
+				["PET_OUTGOING_PARRY"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_DODGE"] = {
+					["message"] = "Dodge!",
+				},
+				["NOTIFICATION_POWER_LOSS"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_SPELL_PARRY"] = {
+					["message"] = "Parry! (%s)",
+				},
+				["PET_OUTGOING_SPELL_IMMUNE"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_SPELL_EVADE"] = {
+					["message"] = "Evade! (%s)",
+					["fontSize"] = 14,
+					["outlineIndex"] = 2,
+					["scrollArea"] = "Custom2",
+					["fontName"] = "Friz Quadrata TT",
+					["fontAlpha"] = 100,
+				},
+				["INCOMING_ABSORB"] = {
+					["message"] = "Abs.! <%a>",
+				},
+				["NOTIFICATION_MONSTER_EMOTE"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_COMBAT_LEAVE"] = {
+					["message"] = "-Battle",
+					["colorB"] = 0,
+					["scrollArea"] = "Custom3",
+					["colorR"] = 0.988235294117647,
+				},
+				["NOTIFICATION_ITEM_BUFF"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_ENEMY_BUFF"] = {
+					["disabled"] = true,
+					["scrollArea"] = "Notification",
+				},
+				["OUTGOING_PARRY"] = {
+					["message"] = "Parry!",
+				},
+				["NOTIFICATION_BUFF"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_DODGE"] = {
+					["message"] = "Dodge!",
+				},
+				["PET_OUTGOING_SPELL_RESIST"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_CP_GAIN"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_PARRY"] = {
+					["message"] = "Parry!",
+				},
+				["NOTIFICATION_MONEY"] = {
+					["message"] = "%e",
+					["fontSize"] = 13,
+				},
+				["INCOMING_SPELL_ABSORB"] = {
+					["message"] = "(%s) Abs! <%a>",
+				},
+				["NOTIFICATION_REP_GAIN"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_DEFLECT"] = {
+					["message"] = "Reflect!",
+				},
+				["NOTIFICATION_PET_COOLDOWN"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_BUFF_STACK"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_SPELL_REFLECT"] = {
+					["message"] = "(%s) Reflect!",
+				},
+				["NOTIFICATION_DEBUFF_FADE"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_SKILL_GAIN"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_DEFLECT"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_ALT_POWER_LOSS"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_HOLY_POWER_CHANGE"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_EVADE"] = {
+					["message"] = "Miss!",
+				},
+				["INCOMING_BLOCK"] = {
+					["message"] = "Blocked!",
+				},
+				["OUTGOING_SPELL_DODGE"] = {
+					["message"] = "Dodge.! (%s)",
+				},
+				["OUTGOING_SPELL_ABSORB"] = {
+					["message"] = "<%a> Abs! (%s)",
+				},
+				["PET_OUTGOING_HOT"] = {
+					["message"] = "*+%a (%s - %n)",
+				},
+				["PET_OUTGOING_SPELL_MISS"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_CP_FULL"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_SHADOW_ORBS_FULL"] = {
+					["message"] = "5!",
+					["alwaysSticky"] = false,
+				},
+				["PET_OUTGOING_IMMUNE"] = {
+					["message"] = "* Невоспр.",
+				},
+				["PET_OUTGOING_SPELL_DAMAGE_SHIELD_CRIT"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_DAMAGE_CRIT"] = {
+					["message"] = "*%a",
+				},
+				["PET_OUTGOING_SPELL_DAMAGE_CRIT"] = {
+					["message"] = "* %a (%s)",
+				},
+				["NOTIFICATION_HOLY_POWER_FULL"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_SPELL_DODGE"] = {
+					["message"] = "(%s)Dodge!",
+				},
+				["PET_OUTGOING_DODGE"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_SPELL_PARRY"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_SPELL_DODGE"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_SPELL_INTERRUPT"] = {
+					["fontSize"] = 14,
+					["outlineIndex"] = 2,
+					["fontName"] = "Friz Quadrata TT",
+					["message"] = "Kicked! (%s)",
+					["fontAlpha"] = 100,
+					["colorG"] = 0.290196078431373,
+					["scrollArea"] = "Custom2",
+					["colorB"] = 0.0313725490196078,
+					["soundFile"] = "Interface\\Addons\\SharedMedia_MyMedia\\sound\\Ding6.ogg",
+				},
+				["OUTGOING_ABSORB"] = {
+					["message"] = "%a (%s)",
+				},
+				["NOTIFICATION_CHI_CHANGE"] = {
+					["disabled"] = true,
+				},
+				["PET_OUTGOING_SPELL_DAMAGE_SHIELD"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_SHADOW_ORBS_CHANGE"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_SPELL_BLOCK"] = {
+					["message"] = "(%s) Block!",
+				},
+				["NOTIFICATION_COMBAT_ENTER"] = {
+					["message"] = "+Battle",
+					["colorB"] = 0,
+					["scrollArea"] = "Custom3",
+					["colorR"] = 0.988235294117647,
+				},
+				["NOTIFICATION_PC_KILLING_BLOW"] = {
+					["message"] = "Final hit! (%n)",
+				},
+				["PET_OUTGOING_DISPEL"] = {
+					["message"] = "* Рассеяно! (%s)",
+					["disabled"] = true,
+				},
+				["INCOMING_HEAL_CRIT"] = {
+					["fontAlpha"] = 100,
+					["fontSize"] = 16,
+					["outlineIndex"] = 3,
+					["fontName"] = "Friz Quadrata TT",
+				},
+				["PET_OUTGOING_SPELL_DOT"] = {
+					["message"] = "* %a (%s)",
+				},
+				["NOTIFICATION_REP_LOSS"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_DEBUFF_STACK"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_MISS"] = {
+					["message"] = "Miss!",
+				},
+				["NOTIFICATION_POWER_GAIN"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_SPELL_RESIST"] = {
+					["message"] = "Resist.! (%s)",
+					["fontSize"] = 14,
+					["outlineIndex"] = 2,
+					["scrollArea"] = "Custom2",
+					["fontName"] = "Friz Quadrata TT",
+					["fontAlpha"] = 100,
+				},
+				["OUTGOING_SPELL_DEFLECT"] = {
+					["message"] = "Reflect.! (%s)",
+					["scrollArea"] = "Custom2",
+				},
+				["OUTGOING_DISPEL"] = {
+					["message"] = "Dispelled! (%s)",
+					["fontSize"] = 14,
+					["outlineIndex"] = 2,
+					["scrollArea"] = "Custom2",
+					["fontName"] = "Friz Quadrata TT",
+					["fontAlpha"] = 100,
+				},
+				["NOTIFICATION_ITEM_COOLDOWN"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_SPELL_DEFLECT"] = {
+					["message"] = "(%s) Reflect!",
+				},
+				["INCOMING_SPELL_INTERRUPT"] = {
+					["message"] = "(%s) Interruped!",
+					["soundFile"] = "oRA3: Twinkle",
+					["alwaysSticky"] = true,
+				},
+				["PET_OUTGOING_EVADE"] = {
+					["message"] = "* Мимо",
+				},
+				["OUTGOING_MISS"] = {
+					["message"] = "Miss!",
+				},
+				["OUTGOING_IMMUNE"] = {
+					["message"] = "Invuln!",
+					["fontSize"] = 14,
+					["outlineIndex"] = 2,
+					["fontName"] = "Friz Quadrata TT",
+					["fontAlpha"] = 100,
+				},
+				["PET_OUTGOING_SPELL_EVADE"] = {
+					["message"] = "* Мимо! (%s)",
+				},
+				["OUTGOING_BLOCK"] = {
+					["message"] = "Block!",
+				},
+				["INCOMING_SPELL_IMMUNE"] = {
+					["message"] = "(%s) Invuln!",
+				},
+				["PET_OUTGOING_SPELL_BLOCK"] = {
+					["disabled"] = true,
+				},
+				["OUTGOING_SPELL_REFLECT"] = {
+					["message"] = "Reflect! (%s)",
+					["fontSize"] = 14,
+					["outlineIndex"] = 2,
+					["scrollArea"] = "Custom2",
+					["fontName"] = "Friz Quadrata TT",
+				},
+				["NOTIFICATION_COOLDOWN"] = {
+					["disabled"] = true,
+				},
+				["NOTIFICATION_EXTRA_ATTACK"] = {
+					["disabled"] = true,
+				},
+				["INCOMING_SPELL_RESIST"] = {
+					["message"] = "(%s) Resist!",
+				},
+				["INCOMING_IMMUNE"] = {
+					["message"] = "Invuln!",
+				},
+				["PET_INCOMING_HOT"] = {
+					["message"] = "(%s - %n) * +%a",
+				},
+				["PET_OUTGOING_ABSORB"] = {
+					["disabled"] = true,
+				},
+				["PET_INCOMING_HEAL_CRIT"] = {
+					["message"] = "(%s - %n) * +%a",
+				},
+				["PET_INCOMING_HEAL"] = {
+					["message"] = "(%s - %n) * +%a",
+				},
+				["NOTIFICATION_DEBUFF"] = {
+					["disabled"] = true,
+				},
+			},
+			["scrollAreas"] = {
+				["Notification"] = {
+					["scrollHeight"] = 130,
+					["offsetX"] = -173,
+					["animationSpeed"] = 40,
+					["offsetY"] = 143,
+				},
+				["Custom2"] = {
+					["direction"] = "Up",
+					["scrollHeight"] = 50,
+					["animationSpeed"] = 60,
+					["stickyDirection"] = "Up",
+					["scrollWidth"] = 130,
+					["offsetX"] = -64,
+					["name"] = "interupts",
+					["offsetY"] = -103,
+					["animationStyle"] = "Static",
+					["stickyAnimationStyle"] = "Static",
+				},
+				["Outgoing"] = {
+					["animationSpeed"] = 20,
+					["behavior"] = "MSBT_NORMAL",
+					["stickyBehavior"] = "MSBT_NORMAL",
+					["stickyDirection"] = "Down",
+					["scrollHeight"] = 110,
+					["offsetX"] = 144,
+					["scrollWidth"] = 100,
+					["iconAlign"] = "Left",
+					["offsetY"] = -450,
+					["animationStyle"] = "Straight",
+					["stickyAnimationStyle"] = "Static",
+				},
+				["Static"] = {
+					["scrollHeight"] = 100,
+					["offsetX"] = -74,
+					["animationSpeed"] = 70,
+					["offsetY"] = -450,
+					["scrollWidth"] = 160,
+				},
+				["Custom3"] = {
+					["critFontAlpha"] = 100,
+					["animationSpeed"] = 50,
+					["skillIconsDisabled"] = true,
+					["critFontSize"] = 20,
+					["scrollWidth"] = 90,
+					["offsetX"] = -40,
+					["name"] = "Battle",
+					["normalFontAlpha"] = 100,
+					["offsetY"] = 0,
+					["scrollHeight"] = 125,
+					["normalFontSize"] = 20,
+				},
+				["Incoming"] = {
+					["animationSpeed"] = 20,
+					["stickyBehavior"] = "MSBT_NORMAL",
+					["behavior"] = "MSBT_NORMAL",
+					["stickyDirection"] = "Down",
+					["scrollHeight"] = 110,
+					["offsetX"] = -242,
+					["scrollWidth"] = 100,
+					["iconAlign"] = "Right",
+					["offsetY"] = -450,
+					["animationStyle"] = "Straight",
+					["stickyAnimationStyle"] = "Static",
+				},
+			},
+			["hideMergeTrailer"] = true,
+			["normalFontName"] = "Arial Narrow",
+			["qualityExclusions"] = {
+				true, -- [1]
+				true, -- [2]
+				true, -- [3]
+				true, -- [4]
+			},
+		},
+	},
+}
+MSBT_SavedMedia = {
+	["fonts"] = {
+	},
+	["sounds"] = {
+		["Ding2"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Ding2.ogg",
+		["Ding1"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Ding1.ogg",
+		["Pling1"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Pling1.ogg",
+		["Ding4"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Ding4.ogg",
+		["Ding3"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Ding3.ogg",
+		["Pling6"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Pling6.ogg",
+		["Ding5"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Ding5.ogg",
+		["Ding7"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Ding7.ogg",
+		["Ding6"] = "Interface\\AddOns\\MikScrollingBattleText\\Sounds\\Ding6.ogg",
+	},
+}
