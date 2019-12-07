83dca75365928f7ba0ea889ecfb40ad1819a922f
Afenar <afenar@gmail.com>
2019-12-07 10:13:52 +0200

    UI

---
+++ AfenarUI_Basic/Kui_Nameplates_Core.lua
@@ -0,0 +1,251 @@
+
+KuiNameplatesCoreSaved = {
+	["216_HEALTH_TRANSITION"] = true,
+	["2162_PERSONAL_FRAME_SIZE_TRANSITION"] = true,
+	["profiles"] = {
+		["AfenarUI"] = {
+			["fade_avoid_execute_friend"] = true,
+			["bot_vertical_offset"] = -5,
+			["frame_height_personal"] = 25,
+			["colour_tapped"] = {
+				0.447058823529412, -- [1]
+				0.43921568627451, -- [2]
+				0.501960784313726, -- [3]
+			},
+			["ignore_uiscale"] = true,
+			["health_text"] = true,
+			["frame_width_personal"] = 80,
+			["clickthrough_self"] = true,
+			["guild_text_npcs"] = false,
+			["fade_speed"] = 1,
+			["threat_brackets"] = true,
+			["target_arrows"] = true,
+			["powerbar_height"] = 10,
+			["classpowers_enable"] = false,
+			["cvar_clamp_bottom"] = 0.13,
+			["frame_height_minus"] = 15,
+			["mouseover_glow"] = true,
+			["colour_neutral"] = {
+				1, -- [1]
+				0.6980392156862745, -- [2]
+				0.3843137254901961, -- [3]
+			},
+			["frame_glow_size"] = 10,
+			["colour_absorb"] = {
+				0.2, -- [1]
+				0.552941176470588, -- [2]
+				1, -- [3]
+				0.8, -- [4]
+			},
+			["auras_icon_minus_size"] = 32,
+			["font_face"] = "Expressway",
+			["fade_conditional_alpha"] = 1,
+			["castbar_colour"] = {
+				0.694117647058824, -- [1]
+				1, -- [2]
+				0.333333333333333, -- [3]
+			},
+			["nameonly_enemies"] = false,
+			["cvar_clamp_top"] = 0.1,
+			["cvar_overlap_v"] = 1.20000004768372,
+			["auras_icon_normal_size"] = 39,
+			["execute_colour"] = {
+				0.866666666666667, -- [1]
+				0.325490196078431, -- [2]
+				1, -- [3]
+			},
+			["level_text"] = true,
+			["castbar_showfriend"] = false,
+			["cvar_name_only"] = true,
+			["castbar_unin_colour"] = {
+				1, -- [1]
+				0, -- [2]
+				0.133333333333333, -- [3]
+			},
+			["name_vertical_offset"] = -4,
+			["font_size_small"] = 12,
+			["nameonly_health_colour"] = false,
+			["fade_non_target_alpha"] = 1,
+			["bar_animation"] = 2,
+			["bar_texture"] = "ElvUI Blank",
+			["auras_offset"] = 4,
+			["colour_hated"] = {
+				1, -- [1]
+				0.372549019607843, -- [2]
+				0.192156862745098, -- [3]
+			},
+			["auras_cd_offset_x"] = -3,
+			["glow_as_shadow"] = false,
+			["target_glow_colour"] = {
+				0.725490196078431, -- [1]
+				0.670588235294118, -- [2]
+				1, -- [3]
+				1, -- [4]
+			},
+			["cvar_personal_show_combat"] = false,
+			["mouseover_glow_colour"] = {
+				0.745098039215686, -- [1]
+				0.71764705882353, -- [2]
+				1, -- [3]
+				1, -- [4]
+			},
+			["frame_width"] = 90,
+			["auras_cd_size"] = 14,
+			["cvar_occluded_mult"] = 0.600000023841858,
+			["classpowers_size"] = 16,
+			["colour_friendly"] = {
+				0.780392156862745, -- [1]
+				1, -- [2]
+				0.427450980392157, -- [3]
+			},
+			["auras_on_personal"] = false,
+			["colour_player"] = {
+				0.2, -- [1]
+				0.713725490196079, -- [2]
+				1, -- [3]
+			},
+			["fade_all"] = true,
+			["frame_height"] = 25,
+			["nameonly_damaged_friends"] = false,
+			["fade_avoid_nameonly"] = false,
+			["frame_width_minus"] = 90,
+			["auras_time_threshold"] = 10,
+			["target_arrows_size"] = 25,
+			["health_text_hostile_max"] = 4,
+			["auras_icon_squareness"] = 0.5,
+			["bossmod_clickthrough"] = true,
+			["castbar_height"] = 14,
+			["health_text_friend_max"] = 4,
+			["cvar_enable"] = true,
+			["tankmode_tank_colour"] = {
+				0.647058823529412, -- [1]
+				1, -- [2]
+				0.286274509803922, -- [3]
+			},
+			["fade_neutral_enemy"] = true,
+			["fade_avoid_execute_hostile"] = true,
+		},
+		["default"] = {
+			["fade_avoid_execute_friend"] = true,
+			["bot_vertical_offset"] = -5,
+			["frame_height_personal"] = 25,
+			["colour_tapped"] = {
+				0.447058823529412, -- [1]
+				0.43921568627451, -- [2]
+				0.501960784313726, -- [3]
+			},
+			["ignore_uiscale"] = true,
+			["health_text"] = true,
+			["frame_width_personal"] = 80,
+			["clickthrough_self"] = true,
+			["guild_text_npcs"] = false,
+			["fade_speed"] = 1,
+			["threat_brackets"] = true,
+			["target_arrows"] = true,
+			["powerbar_height"] = 10,
+			["classpowers_enable"] = false,
+			["cvar_clamp_bottom"] = 0.13,
+			["frame_height_minus"] = 15,
+			["mouseover_glow"] = true,
+			["colour_neutral"] = {
+				1, -- [1]
+				0.6980392156862745, -- [2]
+				0.3843137254901961, -- [3]
+			},
+			["frame_glow_size"] = 10,
+			["colour_absorb"] = {
+				0.2, -- [1]
+				0.552941176470588, -- [2]
+				1, -- [3]
+				0.8, -- [4]
+			},
+			["auras_icon_minus_size"] = 32,
+			["font_face"] = "Expressway",
+			["fade_conditional_alpha"] = 1,
+			["castbar_colour"] = {
+				0.694117647058824, -- [1]
+				1, -- [2]
+				0.333333333333333, -- [3]
+			},
+			["nameonly_enemies"] = false,
+			["cvar_clamp_top"] = 0.1,
+			["cvar_overlap_v"] = 1.20000004768372,
+			["auras_icon_normal_size"] = 39,
+			["execute_colour"] = {
+				0.866666666666667, -- [1]
+				0.325490196078431, -- [2]
+				1, -- [3]
+			},
+			["level_text"] = true,
+			["castbar_showfriend"] = false,
+			["cvar_name_only"] = true,
+			["castbar_unin_colour"] = {
+				1, -- [1]
+				0, -- [2]
+				0.133333333333333, -- [3]
+			},
+			["name_vertical_offset"] = -4,
+			["font_size_small"] = 12,
+			["nameonly_health_colour"] = false,
+			["fade_non_target_alpha"] = 1,
+			["bar_animation"] = 2,
+			["bar_texture"] = "ElvUI Blank",
+			["auras_offset"] = 4,
+			["colour_hated"] = {
+				1, -- [1]
+				0.372549019607843, -- [2]
+				0.192156862745098, -- [3]
+			},
+			["auras_cd_offset_x"] = -3,
+			["glow_as_shadow"] = false,
+			["target_glow_colour"] = {
+				0.725490196078431, -- [1]
+				0.670588235294118, -- [2]
+				1, -- [3]
+				1, -- [4]
+			},
+			["cvar_personal_show_combat"] = false,
+			["mouseover_glow_colour"] = {
+				0.745098039215686, -- [1]
+				0.71764705882353, -- [2]
+				1, -- [3]
+				1, -- [4]
+			},
+			["frame_width"] = 90,
+			["auras_cd_size"] = 14,
+			["cvar_occluded_mult"] = 0.600000023841858,
+			["classpowers_size"] = 16,
+			["colour_friendly"] = {
+				0.780392156862745, -- [1]
+				1, -- [2]
+				0.427450980392157, -- [3]
+			},
+			["auras_on_personal"] = false,
+			["colour_player"] = {
+				0.2, -- [1]
+				0.713725490196079, -- [2]
+				1, -- [3]
+			},
+			["fade_all"] = true,
+			["frame_height"] = 25,
+			["nameonly_damaged_friends"] = false,
+			["fade_avoid_nameonly"] = false,
+			["frame_width_minus"] = 90,
+			["auras_time_threshold"] = 10,
+			["target_arrows_size"] = 25,
+			["health_text_hostile_max"] = 4,
+			["auras_icon_squareness"] = 0.5,
+			["bossmod_clickthrough"] = true,
+			["castbar_height"] = 14,
+			["health_text_friend_max"] = 4,
+			["cvar_enable"] = true,
+			["tankmode_tank_colour"] = {
+				0.647058823529412, -- [1]
+				1, -- [2]
+				0.286274509803922, -- [3]
+			},
+			["fade_neutral_enemy"] = true,
+			["fade_avoid_execute_hostile"] = true,
+		},
+	},
+}
---
+++ AfenarUI_Basic/Kui_SpellList_Config.lua
@@ -0,0 +1,29 @@
+
+KuiSpellListConfigSaved = {
+	["include_own"] = {
+		[204242] = true,
+		[6343] = true,
+		[240842] = true,
+		[257284] = true,
+		[31589] = true,
+		[205708] = true,
+		[116095] = true,
+		[2120] = true,
+		[212792] = true,
+		[1715] = true,
+		[121253] = true,
+		[236027] = true,
+		[213405] = true,
+		[3409] = true,
+		[183218] = true,
+		[45524] = true,
+		[5116] = true,
+		[196840] = true,
+		[271788] = true,
+		[12323] = true,
+	},
+	["include_all"] = {
+	},
+	["exclude"] = {
+	},
+}
